1b578a2825b41636db85e40dd228d3d84d6f4d6d
Marcus Edvardsson <54709946+jensengbg-marcus-edvardsson@users.noreply.github.com>
2020-02-02 16:54:50 +0100

    edits
    edits

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,15 @@
-// Eventlistener on Product-button
+// Eventlisteners
 const button = document.getElementById('hero-button');
 button.addEventListener("click", function(event) {
+    let top = document.getElementById('products-section')
+        top.scrollIntoView({
+            behavior: "smooth", 
+            block: "start"
+        });
     getProducts();
 });
 
-// Getting images from database on localhost:8000/products
-const createNode = (elem) => {
-    return document.createElement(elem);
-};
-
+// Hämta produkter
 const appendNode = (parent, elem) => {
     parent.appendChild(elem);
 };
@@ -20,20 +21,23 @@ const getProducts = async () => {
     .then(response => response.json())
     .then(data => {
         data.forEach((products) => {
-            let card = document.createElement('div'),
-                img = document.createElement('img'),
-                p = document.createElement('p'),
-                span = document.createElement('span');
-                addbutton = document.createElement('button');
-                card.classList.add('product-item');
-                img.classList.add('product-img');
-                p.classList.add('product-name');
-                span.classList.add('product-price')
-                addbutton.classList.add('product-button');
-            img.src = products.img
+            card = document.createElement('div'),
+            img = document.createElement('img'),
+            p = document.createElement('p'),
+            span = document.createElement('span'),
+            addbutton = document.createElement('button');
+
+            card.classList.add('product-item');
+            img.classList.add('product-img');
+            p.classList.add('product-name');
+            span.classList.add('product-price');
+            addbutton.classList.add('product-button');
+
+            img.src = products.img;
             span.innerText = products.price;
             p.innerText = products.name;
             addbutton.innerHTML = "Lägg i varukorg"
+
             appendNode(card, img);
             appendNode(card, p);
             appendNode(card, span);
@@ -50,9 +54,3 @@ const getProducts = async () => {
 // Remove from cart
 
 // CLearar form
-
-let clearForm = () => {
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-    }
-};
